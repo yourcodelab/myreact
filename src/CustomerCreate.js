929580523e1580b0b7e59f4b9b16d828cbc90c5d
Alexssander Antonio Siqueira <alexssander_as@hotmail.com>
2020-03-02 15:16:43 -0500

    Adding REST API methods

--- src/CustomerCreate.js
+++ src/CustomerCreate.js
@@ -1,8 +1,93 @@
 import React, { Component } from "react";
+import CustomerService from "./CustomerService";
 
 class CustomerCreate extends Component {
+  constructor() {
+    super();
+    this.service = new CustomerService();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.state = {
+      id: 0,
+      firstname: "",
+      lastname: ""
+    };
+  }
+
+  onSubmit() {
+    this.service.insert(this.state);
+  }
+
+  onCancel() {
+    this.setState({
+      id: 0,
+      firstname: "",
+      lastname: ""
+    });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    return <div className="CustomerCreate">Create a Customer</div>;
+    return (
+      <div className="input-panel">
+        <span className="form-caption">New Customer:</span>
+        <div>
+          <label className="field-name">
+            ID:
+            <br />
+            <input
+              value={this.state.id}
+              name="id"
+              maxLength="4"
+              required
+              onChange={this.handleInputChange}
+              placeholder="customer ID"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="field-name">
+            First Name:
+            <br />
+            <input
+              value={this.state.firstname}
+              name="firstname"
+              maxLength="40"
+              required
+              onChange={this.handleInputChange}
+              placeholder="customer's fisrt name"
+            />
+          </label>
+        </div>
+        <div>
+          <label className="field-name">
+            Last Name:
+            <br />
+            <input
+              value={this.state.lastname}
+              name="lastname"
+              maxLength="40"
+              required
+              onChange={this.handleInputChange}
+              placeholder="customer's last name"
+            />
+          </label>
+        </div>
+
+        <br />
+        <button onClick={() => this.onCancel()}>Cancel</button>
+        <button onClick={() => this.onSubmit()}>Create</button>
+      </div>
+    );
   }
 }
 
--- src/CustomerService.js
+++ src/CustomerService.js
@@ -1,13 +1,102 @@
 class CustomerService {
   constructor() {
-    this.customers = [
-      { id: 1, firstname: "Alex", lastname: "Siqueira" },
-      { id: 2, firstname: "Pietro", lastname: "Benetti" }
-    ];
+    this.urlservice =
+      "https://raw.githubusercontent.com/yourcodelab/myreact/master/public/customers.json";
   }
 
   async listAll() {
-    return Promise.resolve(this.customers);
+    return fetch(this.urlservice)
+      .then(response => {
+        if (!response.ok) {
+          this.handleResponseError(response);
+        }
+
+        return response.json();
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
+
+  async findByID(id) {
+    return fetch(this.urlservice + "/" + id)
+      .then(response => {
+        if (!response.ok) {
+          this.handleResponseError(response);
+        }
+
+        return response.json();
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
+
+  async insert(customer) {
+    return fetch(this.urlservice, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(customer)
+    })
+      .then(response => {
+        if (!response.ok) {
+          this.handleResponseError(response);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
+
+  async update(customer) {
+    return fetch(this.urlservice, {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(customer)
+    })
+      .then(response => {
+        if (!response.ok) {
+          this.handleResponseError(response);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
+
+  async delete(id) {
+    return fetch(this.urlservice + "/" + id, {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          this.handleResponseError(response);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        this.handleError(error);
+      });
+  }
+
+  handleResponseError(response) {
+    throw new Error("HTTP error, status = " + response.status);
+  }
+
+  handleError(error) {
+    console.log(error.message);
   }
 }
 
